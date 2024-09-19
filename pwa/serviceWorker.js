58d2f3b73a16a775cb8c35f0e4cc72ff744d8468
jz <jz@1.com>
2024-09-19 22:29:05 +0800

    fd

--- pwa/serviceWorker.js
+++ pwa/serviceWorker.js
@@ -1,54 +1,51 @@
 // 缓存静态文件
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-      // 缓存指定文件
-    caches.open('v1').then(function(cache) {
-      return cache.addAll([
-        'index.html',
-        'test.html'
-      ]);
-    })
-  );
+let filesCacheName = 'filesCacheName'
+let allFilesToCache = []
+// install event
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(filesCacheName).then(function (cache) {
+            return cache.addAll(allFilesToCache);
+        })
+    );
 });
- 
 // 缓存接口数据
-self.addEventListener('fetch', function(event) {
-    event.respondWith(caches.match(event.request).then(function(response) {
-      // 匹配到请求
-      if (response !== undefined) {
-        return response;
-      } else {
-        return fetch(event.request).then(function (response) {
-          // 缓存响应数据
-          let responseClone = response.clone();
-  
-          caches.open('v1').then(function (cache) {
-            cache.put(event.request, responseClone);
-          });
-          return response;
-        }).catch(function () {
-          return caches.match('/gallery/myLittleVader.jpg');
-        });
-      }
+self.addEventListener('fetch', function (event) {
+    event.respondWith(caches.match(event.request).then(function (response) {
+        // 匹配到请求
+        if (response !== undefined) {
+            return response;
+        } else {
+            return fetch(event.request).then(function (response) {
+                // 缓存响应数据
+                let responseClone = response.clone();
+
+                caches.open('v1').then(function (cache) {
+                    cache.put(event.request, responseClone);
+                });
+                return response;
+            }).catch(function () {
+                return caches.match('/gallery/myLittleVader.jpg');
+            });
+        }
     }));
-  });
+});
 
 
 // 更新缓存
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function (event) {
     event.waitUntil(
-      caches.keys().then(function(cacheNames) {
-        return Promise.all(
-          cacheNames.map(function(cacheName) {
-            // 如果有更新
-            if (cacheName !== 'v1') {
-              return caches.delete(cacheName);
-            }
-          })
-        );
-      })
-      .then(function(){
-        return self.clients.claim()
-      })
+        // 获取所有 cache 名称
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                // 获取所有不同于当前版本名称 cache 下的内容
+                cacheNames.filter(function (cacheName) {
+                    return cacheName != filesCacheName;
+                }).map(function (cacheName) {
+                    // 删除内容
+                    return caches.delete(cacheName);
+                })
+            );
+        })
     );
-  });
+});
