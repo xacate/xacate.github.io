c735ce2788d58f94f19566dce31f8055ad8e13a4
jz <jz@1.com>
2024-09-20 08:22:24 +0800

    fd

--- pwa/serviceWorker.js
+++ pwa/serviceWorker.js
@@ -1,24 +1,31 @@
 // 缓存静态文件
 let filesCacheName = 'filesCacheName'
-let allFilesToCache = []
+let allFilesToCache = [
+    '/',
+    "index.html",
+    "test.html",
+    "icon.png"
+]
 // install event
 self.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open(filesCacheName).then(function (cache) {
             return cache.addAll(allFilesToCache);
-        })
+        }).then(() => self.skipWaiting())
     );
 });
-self.addEventListener('fetch', function(event) {
+
+// 获取fetch
+self.addEventListener('fetch', function (event) {
     event.respondWith(
         caches.match(event.request)
-            .then(function(response) {
+            .then(function (response) {
                 //该fetch请求已经缓存
-                if (response) {
+                if (response != null) {
                     return response;
                 }
-                return fetch(event.request);
-                }
+                return fetch(event.request.url);
+            }
             )
     );
 });
@@ -27,17 +34,16 @@ self.addEventListener('fetch', function(event) {
 // 更新缓存
 self.addEventListener('activate', function (event) {
     event.waitUntil(
-        // 获取所有 cache 名称
-        caches.keys().then(function (cacheNames) {
-            return Promise.all(
-                // 获取所有不同于当前版本名称 cache 下的内容
-                cacheNames.filter(function (cacheName) {
-                    return cacheName != filesCacheName;
-                }).map(function (cacheName) {
-                    // 删除内容
-                    return caches.delete(cacheName);
+        Promise.all(
+            caches.keys().then(cacheNames => {
+                return cacheNames.map(name => {
+                    if (name !== filesCacheName) {
+                        return caches.delete(name);
+                    }
                 })
-            );
+            })
+        ).then(() => {
+            return self.clients.claim()
         })
-    );
+    )
 });
