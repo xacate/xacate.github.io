54ee92d62f7de6a3a9fc34e61fc2f67b79f9dc05
jz <jz@1.com>
2026-09-07 10:48:34 +0800

    fds

---
+++ tool/tool/fA8_NQWm.js
@@ -0,0 +1,1 @@
+import{N as s,O as u,P as r,Q as o,R as c,S as i}from"./BE_tZlmr.js";function a(t){const e=t||u();return e?.ssrContext?.head||e?.runWithContext(()=>{if(o())return c(i)})}function d(t,e={}){const n=a(e.nuxt);if(n)return s(t,{head:n,...e})}function f(t,e={}){const n=a(e.nuxt);if(n)return r(t,{head:n,...e})}export{f as a,d as u};
